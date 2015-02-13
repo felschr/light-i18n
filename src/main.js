04084515a800509533b1332bf301dd8bfbaf3cb6
Malte-Maurice Dreyer <dreyer.maltem@gmail.com>
2015-02-13 15:22:06 +0000

    Misc changes

--- src/main.js
+++ src/main.js
@@ -1,73 +1,80 @@
 /* light-i18n main */
 /* jshint browser: true */
-
 // Base function.
-(function(global) {
-    "use strict";
-		var language = window.navigator.userLanguage || window.navigator.language,
-						searchTerms = window.location.search.slice(1).split("&");
+(function (global) {
+  "use strict";
 
-		function loadTranslations(language, set, base) {
-				var url = (base || "locales/") + language + "/" + (set || "translation") + ".json";
-
-				console.info("loading translations from: " + url);
-				return fetch(url, {
-						headers: {
-								"Accept": "application/json",
-								"Accept-Encoding": "UTF-8"
-						}
-				}).then(function(res) {
-						if(res.ok) {
-								return res.json();
-						}
-						// TODO add error object instead of false
-						return Promise.reject(false);
-				}).then(function(obj) {
-						console.log("successfully loaded translations");
-						return searchForTranslationElements(obj);
-				}).catch(function(err) {
-						console.error("Error loading translations: %o", err);
-				});
-		}
-
-		function applyTranslation(ele, key, obj) {
-				if (obj) {
-						ele.innerHTML = getByPath(obj, key);
-				} else {
-						console.log("Could not translate website because JSON wasn't loaded yet.");
-				}
-		}
-
-		function getByPath(obj, path) {
-				return path.split(".").reduce(function(obj, key) {
-						return obj ? obj[key] : undefined;
-				}, obj);
-		}
-
-		function searchForTranslationElements(translationJson, parent) {
-				var translationElements = (parent || document).querySelectorAll("[i18n]");
-
-				[].forEach.call(translationElements, function(ele) {
-						applyTranslation(ele, ele.getAttribute("i18n"), translationJson);
-				});
-		}
-
-		searchTerms.some(function(searchTerm) {
-				if (searchTerm.indexOf("lang=") === 0) {
-						language = searchTerm.split("=").slice(1).join("=");
-						return true;
-				}
+  var language = (function(lang) {
+		window.location.search.slice(1).split("&").some(function (searchTerm) {
+			if (searchTerm.indexOf("lang=") === 0) {
+				lang = searchTerm.split("=").slice(1).join("=");
+				return true;
+			}
 		});
 
-		language = language.split("-")[0];
-		loadTranslations(language.toLowerCase());
+		return lang.toLowerCase().split("-")[0];
+	})(window.navigator.userLanguage || window.navigator.language);
 
-		global.i18n = {
-				set language(lang) {
-						language = String(lang);
-				},
-				get language() {
-						return language;
-				}
-		};
-})(this);
+  function loadTranslations(language, set, base) {
+    var url = (base || "locales/") + language + "/" + (set || "translation") + ".json";
+    console.info("loading translations from: " + url);
+
+    return fetch(url, {
+      headers: {
+        "Accept": "application/json",
+        "Accept-Encoding": "UTF-8"
+      }
+    }).then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+
+      // TODO add error object instead of false
+      return Promise.reject(false);
+    }).then(function (obj) {
+      console.log("successfully loaded translations");
+
+      return searchForTranslationElements(obj);
+    }).catch(function (err) {
+      console.error("Error loading translations: %o", err);
+    });
+  }
+
+  function applyTranslation(ele, path, obj) {
+		var translated = getByPath(obj, path);
+
+    if (typeof(translated) !== "undefined") {
+			ele.textContent = translated;
+    } else {
+      console.log("Could not translate website: path '%s' not found", path);
+    }
+  }
+
+  function getByPath(obj, path) {
+    return path.split(".").reduce(function (obj, key) {
+      return obj ? obj[key] : undefined;
+    }, obj);
+  }
+
+  function searchForTranslationElements(obj, parent) {
+    [].slice.call((parent || document).querySelectorAll("[i18n]")).forEach(function(ele) {
+      applyTranslation(ele, ele.getAttribute("i18n"), obj);
+    });
+  }
+
+  loadTranslations(language);
+
+  global.i18n = {
+    set language(lang) {
+			lang = String(lang);
+
+			if(lang !== language) {
+      	language = lang;
+				loadTranslations(lang);
+			}
+    },
+    get language() {
+      return language;
+    }
+  };
+}(this));
