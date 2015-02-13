3f24c9a29fc25278f8fd26b74157c1485b52df2d
Felix Schröter <felschr@gmail.com>
2015-02-13 10:08:15 +0100

    initial code

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # light-i18n
+
 A lightweight, client-based internationalization library for javascript.
 
-The setup is very easy and can be done by anyone who has very basic javascript knowledge.
-The following setup instructions should also make it easy to set up for people who only know HTML.
-This library doesn't depend on other libraries or frameworks.
+## About
 
-##Setup
-###manual setup
+An JavaScript library by Felix Schröter.
+
+See the [project homepage](http://FelschR.github.io/light-i18n).
+
+## Installation
+
+###manual installation
 To embed light-i18n into your HTML website include the following:
 ```HTML
 <script src="scripts/light-i18n.js"></script>
 ```
+
+###using bower
+light-i18n will soon also be downloadable as a bower package. Check back later.
+
+## Usage
+
+Basic usage is as follows:
 
 The translations are saved in a folder called `locales` which is in the root of your website.
 Inside this folder each translation also gets it's own folder which is named after the lowercase language key (de, en, fr, ...). The translation files are called `translation.json`.
@@ -34,7 +45,7 @@ Set up the translations in a JSON file like this:
     "imprint": "Impressum"
   },
   "content" : {
-    "welcomeMsg": "Wilkommen",
+    "welcomeMsg": "Wilkommen"
   }
 }
 ```
@@ -45,5 +56,12 @@ You can set the translation on an HTML element via the attribute `i18n`:
 ```
 The example above will show "Welcome" as default text and if a translation with the key `content.welcomeMsg` is available it will insert the translation to the innerHTML of the element.
 
-###bower
-light-i18n will soon also be downloadable as a bower package. Check back later.
+For advanced usage, see the documentation.
+
+## Documentation
+
+Start with `docs/MAIN.md`.
+
+## License
+
+MIT. See `LICENSE` in this directory.
---
+++ src/main.js
@@ -0,0 +1,91 @@
+/* light-i18n main */
+/* jshint browser: true */
+
+// Base function.
+var i18n = function() {
+	"use strict";
+	var language = window.navigator.userLanguage || window.navigator.language,
+			searchTerms = window.location.search.substring(1).split(/[?&]/);
+
+	function loadTranslations(language) {
+		var url = window.location.protocol + "//" + window.location.host + window.location.pathname + "locales/" + language + "/translation.json",
+				xhr;
+
+		console.log("loading translations from: " + url);
+		xhr = new XMLHttpRequest();
+		xhr.open("GET", url, true);
+		xhr.setRequestHeader("Accept", "application/json");
+		xhr.setRequestHeader("Accept-Encoding", "UTF-8");
+		
+		// TODO add event listeners for error cases
+		xhr.onreadystatechange = function() {
+			var translationJson;
+
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					try {
+						console.log("successfully loaded translations");
+						translationJson = JSON.parse(xhr.responseText);
+					} catch (e) {
+						console.error("could not parse JSON translations", e);
+					}
+
+					if (translationJson) {
+						searchForTranslationElements(translationJson);
+					}
+
+				} else {
+					console.log("failed loading translations");
+				}
+			}
+		};
+		xhr.send();
+	}
+
+	function getTranslation(key, json) {
+		var nodes = key.split("."),
+				translation = json;
+
+		nodes.forEach(function(node) {
+			translation = translation[node];
+		});
+
+		return translation;
+	}
+
+	function applyTranslation(ele, key, json) {
+		if (json !== null) {
+			ele.innerHTML = getTranslation(key, json);
+		} else {
+			console.log("Could not translate website because JSON wasn't loaded yet.");
+		}
+	}
+
+	function searchForTranslationElements(translationJson) {
+		var translationElements = document.querySelectorAll("[i18n]");
+
+		[].forEach.call(translationElements, function(ele) {
+			applyTranslation(ele, ele.getAttribute("i18n"), translationJson);
+		});
+	}
+
+	searchTerms.some(function(searchTerm) {
+		if (searchTerm.indexOf("lang=") === 0) {
+			language = searchTerm.split("=")[1];
+			return true;
+		}
+	});
+
+	language = language.split("-")[0];
+	loadTranslations(language.toLowerCase());
+
+	return true;
+};
+
+
+// Version.
+i18n.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+root.i18n = i18n;
